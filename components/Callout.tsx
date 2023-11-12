c08ee8afb5a253dcf98a526e4a403881f3319bc0
subash <subash@eyemovic.com>
2023-11-12 22:16:12 +0900

    fixed

---
+++ components/Callout.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+type CallOutProps = {
+    children: React.ReactNode;
+};
+
+const Callout: React.FC<CallOutProps> = ({ children }) => {
+    useEffect(() => {}, []);
+
+    return (
+        <div className="nextra-callout nx-overflow-x-auto nx-mt-6 nx-flex nx-rounded-lg nx-border nx-py-2 ltr:nx-pr-4 rtl:nx-pl-4 contrast-more:nx-border-current contrast-more:dark:nx-border-current nx-border-orange-100 nx-bg-orange-50 nx-text-orange-800 dark:nx-border-orange-400/30 dark:nx-bg-orange-400/20 dark:nx-text-orange-300">
+            <div className="nx-select-none nx-text-xl ltr:nx-pl-3 ltr:nx-pr-2 rtl:nx-pr-3 rtl:nx-pl-2">
+                💡
+            </div>
+            <div className="nx-w-full nx-min-w-0 nx-leading-7">{children}</div>
+        </div>
+    );
+};
+
+export default Callout;
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>LangLift Pro</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/subashrijal5/langpro-doc',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/subashrijal5/langpro-doc',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Langlift Pro',
   },
 }
 
